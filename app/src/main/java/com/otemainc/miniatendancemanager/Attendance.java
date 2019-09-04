f553baeb894a328e5629b8c58fc5dcbc52d8a467
TheOnlySmartBoy <o2jose43@gmail.com>
2019-09-04 11:19:29 +0300

    Completed the basic checkin checkouot logic

--- app/src/main/java/com/otemainc/miniatendancemanager/Attendance.java
+++ app/src/main/java/com/otemainc/miniatendancemanager/Attendance.java
@@ -2,13 +2,93 @@ package com.otemainc.miniatendancemanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class Attendance extends AppCompatActivity {
+import com.otemainc.miniatendancemanager.db.Db;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
+
+public class Attendance extends AppCompatActivity implements View.OnClickListener {
+    EditText staff;
+    Button signIn, signOut;
+    TextView status;
+    Db myDb;
+    SharedPreferences sharedpreferences;
+    public static final String mypreference = "mypref";
+    public static final String Name = "nameKey";
+    public static final String Status = "statusKey";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attendance);
+        staff= findViewById(R.id.txtNo);
+        signIn = findViewById(R.id.btnCheck);
+        signOut = findViewById(R.id.btnSignOut);
+        status = findViewById(R.id.txtStatus);
+        myDb = new Db(this);
+        sharedpreferences = getSharedPreferences(mypreference,
+                Context.MODE_PRIVATE);
+        signIn.setOnClickListener(this);
+        signOut.setOnClickListener(this);
+        if (sharedpreferences.contains(Status)) {
+            status.setText(sharedpreferences.getString(Status, ""));
+            staff.setVisibility(GONE);
+            signIn.setVisibility(GONE);
+            signOut.setVisibility(VISIBLE);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnCheck:
+                Date timeIn = Calendar.getInstance().getTime();
+                String no = staff.getText().toString().trim();
+                boolean signedIn;
+               signedIn= myDb.addTimeIn(no,timeIn.toString());
+               if(signedIn){
+                   SharedPreferences.Editor editor = sharedpreferences.edit();
+                   String statusText = "Working";
+                   editor.putString(Status, statusText);
+                   editor.putString(Name, no);
+                   editor.commit();
+                   status.setText(statusText);
+                   Toast.makeText(this,"Welcome "+ no +" Enjoy you working session", Toast.LENGTH_LONG).show();
+                   staff.setVisibility(GONE);
+                   signIn.setVisibility(GONE);
+                   signOut.setVisibility(VISIBLE);
+               }
+                break;
+            case R.id.btnSignOut:
+                boolean signedOut;
+                String staffNo = sharedpreferences.getString(Name, "");
+                Date timeOut = Calendar.getInstance().getTime();
+                signedOut = myDb.addTimeOut(staffNo,timeOut.toString());
+                if(signedOut) {
+                    SharedPreferences.Editor editor = sharedpreferences.edit();
+                    editor.remove(Status);
+                    editor.remove(Name);
+                    editor.commit();
+                    status.setText(R.string.you_have_not_checked_in_yet);
+                    Toast.makeText(this, "Good bye Hope to see you soon", Toast.LENGTH_LONG).show();
+                    staff.setVisibility(VISIBLE);
+                    signIn.setVisibility(VISIBLE);
+                    signOut.setVisibility(GONE);
+                }
+                break;
+        }
     }
 }
--- app/src/main/java/com/otemainc/miniatendancemanager/db/Db.java
+++ app/src/main/java/com/otemainc/miniatendancemanager/db/Db.java
@@ -9,7 +9,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class Db extends SQLiteOpenHelper {
 
     // If you change the database schema, you must increment the database version.
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 2;
     private static final String DATABASE_NAME = "att.db";
     private static final String SQL_CREATE_USERS_TABLE =  "CREATE TABLE tbl_users (" +
             "id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
@@ -21,15 +21,19 @@ public class Db extends SQLiteOpenHelper {
             "id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
             "name varchar(100) NOT NULL," +
             "descr varchar(255) NOT NULL);";
-    private static final String SQL_CREATE_ATTENDANCE_TABLE =  "CREATE TABLE tbl_attendance (" +
+    private static final String SQL_CREATE_TIMEIN_TABLE =  "CREATE TABLE tbl_timein (" +
             "id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
             "staff varchar(100) NOT NULL," +
-            "timeIn varchar(100),"+
+            "timeIn varchar(100));";
+    private static final String SQL_CREATE_TIMEOUT_TABLE =  "CREATE TABLE tbl_timeout (" +
+            "id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
+            "staff varchar(100) NOT NULL," +
             "timeOut varchar(100));";
 
     private static final String SQL_DELETE_USERS_TABLE = "DROP TABLE IF EXISTS tbl_users";
     private static final String SQL_DELETE_DEPT_TABLE = "DROP TABLE IF EXISTS tbl_dept";
-    private static final String SQL_DELETE_ATTENDANCE_TABLE = "DROP TABLE IF EXISTS tbl_attendance";
+    private static final String SQL_DELETE_TIMEIN_TABLE = "DROP TABLE IF EXISTS tbl_timein";
+    private static final String SQL_DELETE_TIMEOUT_TABLE = "DROP TABLE IF EXISTS tbl_timeout";
 
     public Db(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -38,14 +42,16 @@ public class Db extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(SQL_CREATE_USERS_TABLE);
         db.execSQL(SQL_CREATE_DEPT_TABLE);
-        db.execSQL(SQL_CREATE_ATTENDANCE_TABLE);
+        db.execSQL(SQL_CREATE_TIMEIN_TABLE);
+        db.execSQL(SQL_CREATE_TIMEOUT_TABLE);
     }
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // This database is only a cache for online data, so its upgrade policy is
         // to simply to discard the data and start over
         db.execSQL(SQL_DELETE_USERS_TABLE);
         db.execSQL(SQL_DELETE_DEPT_TABLE);
-        db.execSQL(SQL_DELETE_ATTENDANCE_TABLE);
+        db.execSQL(SQL_DELETE_TIMEIN_TABLE);
+        db.execSQL(SQL_DELETE_TIMEOUT_TABLE);
         onCreate(db);
     }
     public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
@@ -82,13 +88,27 @@ public class Db extends SQLiteOpenHelper {
         }
         return addedDept;
     }
-    public boolean addTimmeIn(String staff, String timeIn){
+    public boolean addTimeIn(String staff, String timeIn){
         boolean addedTimeIn;
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValue = new ContentValues();
         contentValue.put("staff",staff);
         contentValue.put("timeIn",timeIn);
-        long result = db.insert("tbl_attendance",null,contentValue);
+        long result = db.insert("tbl_timein",null,contentValue);
+        if(result==-1){
+            addedTimeIn=false;
+        }else{
+            addedTimeIn=true;
+        }
+        return addedTimeIn;
+    }
+    public boolean addTimeOut(String staff, String timeOut){
+        boolean addedTimeIn;
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValue = new ContentValues();
+        contentValue.put("staff",staff);
+        contentValue.put("timeOut",timeOut);
+        long result = db.insert("tbl_timeout",null,contentValue);
         if(result==-1){
             addedTimeIn=false;
         }else{
@@ -117,73 +137,31 @@ public class Db extends SQLiteOpenHelper {
         db.update("tbl_dept",contentValue,"id=?",new String[]{id});
         return true;
     }
-    public boolean AddTimeOut(String id, String staff, String timeIn, String timeOut){
-        boolean addedTimeIn;
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues contentValue = new ContentValues();
-        contentValue.put("id",id);
-        contentValue.put("staff",staff);
-        contentValue.put("timeIn",timeIn);
-        contentValue.put("timeOut", timeOut);
-        db.update("tbl_attendance", contentValue,"id=?",new String[]{id});
-                return true;
-    }
+
     //Retrieve
     public Cursor getAllUsers(){
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor res = db.rawQuery("Select * from tbl_users",null);
-        return  res;
+        return db.rawQuery("Select * from tbl_users",null);
 
     }
     public Cursor Login(String email, String pass){
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor res = db.query("tbl_users",null,"email=? AND password=?",new String[]{email,pass},null,null,null);
-        return res;
+        return db.query("tbl_users",null,"email=? AND password=?",new String[]{email,pass},null,null,null);
 
     }
-    /*public boolean checkUser(String email, String password) {
-
-        // array of columns to fetch
-        String[] columns = {
-               "name","dept","email"
-        };
-        SQLiteDatabase db = this.getReadableDatabase();
-        // selection criteria
-        String selection =  " email = ?" + " AND " + COLUMN_USER_PASSWORD + " = ?";
-
-        // selection arguments
-        String[] selectionArgs = {email, password};
-
-        // query user table with conditions
-
-        Cursor cursor = db.query(TABLE_USER, //Table to query
-                columns,                    //columns to return
-                selection,                  //columns for the WHERE clause
-                selectionArgs,              //The values for the WHERE clause
-                null,                       //group the rows
-                null,                       //filter by row groups
-                null);                      //The sort order
-
-        int cursorCount = cursor.getCount();
-
-        cursor.close();
-        db.close();
-        if (cursorCount > 0) {
-            return true;
-        }
-
-        return false;
-    }*/
     public Cursor getAllDepts(){
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor res = db.rawQuery("Select * from tbl_dept",null);
-        return  res;
+        return db.rawQuery("Select * from tbl_dept",null);
 
     }
-    public Cursor getAllAttend(){
+    public Cursor getAllTimeIns(){
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor res = db.rawQuery("Select * from tbl_attendance",null);
-        return  res;
+        return db.rawQuery("Select * from tbl_timein",null);
+
+    }
+    public Cursor getAllTimeOuts(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        return db.rawQuery("Select * from tbl_timeout",null);
 
     }
     //Delete
